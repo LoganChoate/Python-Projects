f1267dd710c2c796e59c31157bd1fdc37142ff88
LoganChoate <loganvchoate@gmail.com>
2021-11-14 20:11:03 -0700

    Create Inheritance_Submission_Assignment.py
    new file for assignment

---
+++ Inheritance_Submission_Assignment.py
@@ -0,0 +1,37 @@
+##example classes
+class User:
+    name = "No Name Provided"
+    email = " "
+    password = "1234abcd"
+    account_number = 0
+
+class Employee(User):
+    base_play = 11.00
+    department = "General"
+
+class Customer(User):
+    mailing_address = " "
+    mailing_list = True
+
+## this is the code for the Inheritance Submission Assignment
+class Car:
+    wheels = 0
+    color = " "
+    num_engine_cylinders = 0
+    headlights = 0
+    brand = " "
+
+class Sports_Car:
+    wheels = 6
+    color = "red"
+    num_engine_cylinders = 12
+    headlights = 4
+    brand = "X-treme Spdr 2"
+
+class Family_Car:
+    wheels = 4
+    color = "white"
+    num_engine_cylinder = 4
+    headlights = 2
+    brand = "Baby Wagon 9000"
+    
