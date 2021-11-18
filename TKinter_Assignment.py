403ca7f93acd8e19855c68f6b9a8983a55fcc2c5
LoganChoate <loganvchoate@gmail.com>
2021-11-17 21:19:45 -0700

    Create TKinter_Assignment.py
    created a file for the TKinter assignment

---
+++ TKinter_Assignment.py
@@ -0,0 +1,23 @@
+##this code imports the relevant libraries
+import tkinter
+from tkinter import *
+
+##this code is the start of several examples of using TKinter
+class ParentWindow(Frame):
+    def __init__(self, master):
+        Frame.__init__(self)
+        
+        self.master = master
+        self.master.resizable(width = False, height = False)
+        self.master.geometry("{}x{}".format(700, 400))
+        self.master.title("Learning TKinter!")
+
+
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
